9b1b5074916491b13898e03be9bb9307e53a775f
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-05-08 23:38:30 +0900

    update like view

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,15 @@
 class LikesController < ApplicationController
-  before_action :set_group
-  before_action :set_message
   
   def create
     @like = Like.create(user_id: current_user.id, group_id: params[:group_id], message_id: params[:message_id])
+    @group = Group.find(params[:group_id])
+    @message = Message.find(params[:message_id])
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, group_id: params[:group_id], message_id: params[:message_id])
     like.destroy
-  end
-
-  
-  private
-
-  def set_group
     @group = Group.find(params[:group_id])
-  end
-
-  def set_message
     @message = Message.find(params[:message_id])
   end
 
